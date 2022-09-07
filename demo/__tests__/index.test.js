4c3c0592cbfd71e3d2d95b48aff9982ae42b8582
Daniel Barclay <daniel.barclay@lloydsbanking.com>
2022-09-07 13:12:56 +0100

    Demonstrate integration test failure

--- demo/__tests__/index.test.js
+++ demo/__tests__/index.test.js
@@ -1,3 +1,12 @@
-it('can run a test', () => {
-  expect(true).toEqual(true);
+import { fireEvent, screen } from '@testing-library/react';
+import { renderWithRouterWrapper } from './utils';
+
+it('can render multiple routes', () => {
+  renderWithRouterWrapper({ route: '/' });
+
+  expect(screen.getByRole('heading', { name: 'Welcome' })).toBeVisible();
+
+  fireEvent.click(screen.getByRole('link', { name: 'Invoices' }));
+
+  expect(screen.getByRole('heading', { name: 'Invoices' })).toBeVisible();
 });
--- demo/src/components/Nav.jsx
+++ demo/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from '@reach/router';
 
 export const Nav = () => (
   <>
-    <h1>Tutorial!</h1>
+    <h1>Demo!</h1>
     <nav>
       <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>{' '}
       <Link to="invoices">Invoices</Link>
